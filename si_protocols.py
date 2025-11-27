c966fd1b7424ef3d51d537a4996a628b0adbef51
chrishalcrow <chrishalcrow@gmail.com>
2025-11-27 11:29:08 +0000

    add files

---
+++ si_protocols.py
@@ -0,0 +1,173 @@
+from pathlib import Path
+
+import spikeinterface.full as si
+from spikeinterface.curation.curation_tools import resolve_merging_graph
+from spikeinterface.curation import validate_curation_dict
+
+protocols = {
+
+    'kilosort4A': {
+        'preprocessing': {
+            'detect_and_remove_bad_channels': {'seed': 1205},
+            'phase_shift': {},
+        },
+        'sorting': {
+            'sorter_name': 'kilosort4',
+            'do_correction': False,
+            'use_binary_file': False,
+        },
+        'preprocessing_for_analyzer': {
+            'detect_and_remove_bad_channels': {'seed': 1205},
+            'phase_shift': {},
+            'common_reference': {},
+            'bandpass_filter': {},
+        },
+    },
+
+    'mountainsort5A': {
+        'preprocessing': {
+            # 'bandpass_filter': {},
+            # 'whiten': {'dtype': 'float'},
+        },
+        'sorting': {
+            'sorter_name': 'mountainsort5',
+            'scheme': '2',
+        },
+        'preprocessing_for_analyzer': {
+            'common_reference': {},
+            'bandpass_filter': {},
+        },
+    },
+
+    'mountainsort5B': {
+        'preprocessing': {
+            # 'bandpass_filter': {},
+            # 'whiten': {'dtype': 'float'},
+        },
+        'sorting': {
+            'sorter_name': 'mountainsort5',
+            'scheme': '3',
+        },
+        'preprocessing_for_analyzer': {
+            'common_reference': {},
+            'bandpass_filter': {},
+        },
+    },
+
+    'mountainsort4A': {
+        'preprocessing': {
+        },
+        'sorting': {
+            'sorter_name': 'mountainsort4',
+        },
+        'preprocessing_for_analyzer': {
+            'common_reference': {},
+            'bandpass_filter': {},
+        },
+    },
+
+    'herdingspikesA': {
+        'preprocessing': {
+            'bandpass_filter': {},
+            'common_reference': {},
+     #       'whiten': {'dtype': 'float'},
+        },
+        'sorting': {
+            'sorter_name': 'herdingspikes',
+        },
+        'preprocessing_for_analyzer': {
+            'bandpass_filter': {},
+            'common_reference': {},
+        },
+    },
+
+    'spykingcircus2A': {
+        'preprocessing': {
+        },
+        'sorting': {
+            'sorter_name': 'spykingcircus2',
+            'apply_motion_correction': False,
+        },
+        'preprocessing_for_analyzer': {
+            'bandpass_filter': {},
+            'common_reference': {},
+        },
+    },
+
+    'tridesclous2A': {
+        'preprocessing': {
+        },
+        'sorting': {
+            'sorter_name': 'tridesclous2',
+        },
+        'preprocessing_for_analyzer': {
+            'bandpass_filter': {},
+            'common_reference': {},
+        },
+    },
+
+
+}
+
+generic_postprocessing = {
+    'unit_locations': {},
+    'random_spikes': {},
+    'noise_levels': {},
+    'waveforms': {},
+    'templates': {},
+    'spike_amplitudes': {'peak_sign': 'both'},
+    'isi_histograms': {},
+    'spike_locations': {'spike_retriver_kwargs': {'peak_sign': 'both'}},
+    'correlograms': {},
+    'template_similarity': {'method': 'l2'},
+    'quality_metrics': {'metric_names': ['num_spikes', 'firing_rate', 'presence_ratio', 'snr', 'isi_violation', 'rp_violation', 'sliding_rp_violation', 'amplitude_cutoff', 'amplitude_median', 'amplitude_cv', 'synchrony', 'firing_range', 'drift', 'sd_ratio'], 'metric_params': {'snr': {'peak_sign': 'both'}, 'amplitude_cutoff': {'peak_sign': 'both'}, 'amplitude_median': {'peak_sign': 'both'}}},
+    'template_metrics': {'include_multi_channel_metrics': True, 'peak_sign': 'both'},
+}
+
+
+def compute_automated_curation(analyzer, model_path, curation_output_path):
+
+    unitrefine_labels = si.auto_label_units(analyzer, model_folder=model_path, trust_model=True)
+
+    noise_units = list(unitrefine_labels[unitrefine_labels['prediction'] == 'noise'].index)
+
+    label_definitions = {
+        "quality": dict(name="quality", label_options=["good", "noise"], exclusive=True),
+    }
+
+    manual_labels = [
+        {"unit_id": unit_id, "labels": {"quality": [unitrefine_labels.loc[unit_index].values[0]]}}
+        for unit_index, unit_id in enumerate(analyzer.unit_ids)
+    ]
+
+    potential_merges = si.compute_merge_unit_groups(
+        analyzer.select_units(list(unitrefine_labels.query('prediction == "good"').index)),
+        preset='x_contaminations'
+    )
+    more_potential_merges = si.compute_merge_unit_groups(
+        analyzer.select_units(list(unitrefine_labels.query('prediction == "good"').index)),
+        preset="temporal_splits"
+    )
+
+    final_merges = resolve_merging_graph(analyzer.sorting, potential_merges + more_potential_merges)
+
+    curation_dict = dict(
+        format_version="2",
+        unit_ids=analyzer.unit_ids,
+        label_definitions=label_definitions, 
+        merges=[
+            dict(unit_ids=p)
+            for p in final_merges
+        ],
+        removed=noise_units,
+        manual_labels=manual_labels
+    )
+
+    validate_curation_dict(curation_dict)
+
+    # we can use the CurationModel for more secure serialization
+    from spikeinterface.curation.curation_model import CurationModel
+
+    curation = CurationModel(**curation_dict)
+    curation_file = Path(curation_output_path)
+    _ = curation_file.write_text(curation.model_dump_json(indent=4))
---
+++ sort_on_comp.py
@@ -0,0 +1,87 @@
+from argparse import ArgumentParser
+from pathlib import Path
+from nolanlab_ephys.si_protocols import protocols
+from nolanlab_ephys.utils import get_recording_folders, chronologize_paths
+from nolanlab_ephys.probe_info import make_probe_plot
+from nolanlab_ephys.si_protocols import generic_postprocessing, compute_automated_curation
+
+import spikeinterface.full as si
+
+def main():
+
+    parser = ArgumentParser()
+
+    parser.add_argument('mouse')
+    parser.add_argument('day')
+    parser.add_argument('sessions')
+    parser.add_argument('protocols')
+    parser.add_argument('--data_folder', default=None)
+    parser.add_argument('--deriv_folder', default=None)
+
+    mouse = int(parser.parse_args().mouse)
+    day = int(parser.parse_args().day)
+
+    sessions_string = parser.parse_args().sessions
+    sessions = sessions_string.split(',')
+
+    protocols = parser.parse_args().protocols
+    protocols_list = protocols.split(',')
+
+    data_folder = parser.parse_args().data_folder
+    if data_folder is None:
+        data_folder = "/home/nolanlab/Work/Harry_Project/data/"
+    data_folder = Path(data_folder)
+
+    deriv_folder = parser.parse_args().deriv_folder
+    if deriv_folder is None:
+        deriv_folder = "/home/nolanlab/Work/Harry_Project/derivatives/"
+    deriv_folder = Path(deriv_folder)
+
+    mouseday_deriv_folder = deriv_folder / f"M{mouse:02d}/D{day:02d}"
+    mouseday_deriv_folder.mkdir(parents=True, exist_ok=True)
+
+    si.set_global_job_kwargs(n_jobs=8)
+
+    for protocol in protocols_list:
+        _ = do_sorting_pipeline(mouse, day, sessions, data_folder, deriv_folder, protocol)
+
+def do_sorting_pipeline(mouse, day, sessions, data_folder, deriv_folder, protocol):
+
+    protocol_info = protocols[protocol]
+
+    recording_paths = chronologize_paths(get_recording_folders(data_folder=data_folder, mouse =mouse, day=day))
+
+    make_probe_plot(recording_paths[0], save_path=deriv_folder / f"M{mouse:02d}/D{day:02d}/M{mouse:02d}_D{day:02d}_probe_layout.png")
+
+    recordings = [si.read_openephys(recording_path) for recording_path in recording_paths]
+
+    concatenated_recording = si.concatenate_recordings(recordings)
+
+    pp_recording = si.apply_preprocessing_pipeline(concatenated_recording, protocol_info['preprocessing'])
+
+    sorting = si.run_sorter(recording=pp_recording, **protocol_info['sorting'], remove_existing_folder=True, verbose=True, folder=f"M{mouse:02d}_D{day:02d}_{protocol}_{'-'.join(sessions)}_output")
+
+    for recording, session in zip(recordings, sessions):
+
+        # we do all our syncing assuming that t=0 is at the start of the ephys data
+        recording._recording_segments[0].t_start = 0
+
+        analyzer = si.create_sorting_analyzer(
+            recording=si.apply_preprocessing_pipeline(recording, protocol_info['preprocessing_for_analyzer']), 
+            sorting=sorting, 
+            folder = deriv_folder / f"M{mouse:02d}/D{day:02d}/{session}/{protocol}/sub-{mouse:02d}_day-{day:02d}_ses-{session}_srt-{protocol}_analyzer",
+            format = "zarr",
+            peak_sign = "both",
+            radius_um = 70,
+        )
+
+        analyzer.compute(generic_postprocessing)
+
+    unitrefine_model_path = "/exports/eddie/scratch/chalcrow/wolf/code/models/curationA_model"
+    curation_output_path = deriv_folder / f"M{mouse:02d}/D{day:02d}/{session}/{protocol}/sub-{mouse:02d}_day-{day:02d}_ses-{session}_srt-{protocol}_curationA.json"
+    compute_automated_curation(analyzer, unitrefine_model_path, curation_output_path=curation_output_path)
+
+    return analyzer
+
+if __name__ == "__main__":
+    main()
